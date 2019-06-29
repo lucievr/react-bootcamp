c36d75cbf4fe1988c6a98a5c6aa26919ed00f04a
Lucie <46979603+lucievr@users.noreply.github.com>
2019-06-29 14:21:44 +0200

    Add more colors

--- 10-color-boxes/BoxContainer.js
+++ 10-color-boxes/BoxContainer.js
@@ -1,7 +1,20 @@
 class BoxContainer extends React.Component {
     static defaultProps = {
-        numBoxes: 18,
-        allColors: ["purple", "magenta", "violet", "pink"]
+        numBoxes: 20,
+        allColors : [
+			'#BA68C8',
+			'#9575CD',
+			'#7986CB',
+			'#9C27B0',
+			'#673AB7',
+			'#3F51B5',
+			'#7B1FA2',
+			'#512DA8',
+			'#303F9F',
+			'#E91E63',
+			'#F06292',
+			'#C2185B'
+		]
     };
     
     render() {
